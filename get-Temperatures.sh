fb88efd5291d53264d5b9f8b0763b606b80dfbf2
Michael Mäder <mike@moik.org>
2016-06-11 18:54:20 +0200

    time period now correctly calculated

--- get-Temperatures.sh
+++ get-Temperatures.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
+_FROM=$(date -v-1H "+%d.%m.%Y+%H:00:00") 
+_TO=$(date -v-1H "+%d.%m.%Y+%H:59:59")
+_FROME=$(date -j -f "%d.%m.%Y+%H:%M:%S" $_FROM "+%s")
+_TOE=$(date -j -f "%d.%m.%Y+%H:%M:%S" $_TO "+%s")
 
-_FROM="03.06.2016+00%3A00"
-_FROME=1464904805
-_TO="03.06.2016+23%3A59"
-_TOE=1464991140
+echo $_FROM
+echo $_TO
+echo $_FROME
+echo $_TOE
 
 source ./credentials.sh
 
 curl -i -s -k  -X 'POST' \
 	-b "PhoneID=$PHONEID" \
  	--data-binary $"deviceid=$_DEVICEID&vendorid=C7BB7FBE-BCEB-48D0-9A49-764A24A6E545&appbundle=de.synertronixx.remotemonitor&fromepoch=$_FROME&toepoch=$_TOE&from=$_FROM&to=$_TO&pagesize=250"\
- 	'http://measurements.mobile-alerts.eu/Home/MeasurementDetails' | \
-	grep -i -e '</\?TABLE\|</\?TD\|</\?TR\|</\?TH' | \
-	sed 's/^[\ \t]*//g' \
-	| tr -d '\n\r' \
-	| sed 's/<\/TR[^>]*>/\n/Ig' \
-	| sed 's/<\/\?\(TABLE\|TR\)[^>]*>//Ig' \
-	| sed 's/^<T[DH][^>]*>\|<\/\?T[DH][^>]*>$//Ig' \
-	| sed 's/<\/T[DH][^>]*><T[DH][^>]*>/;/Ig'
+ 	'http://measurements.mobile-alerts.eu/Home/MeasurementDetails' | 
+	grep -i -e '</\?TABLE\|</\?TD\|</\?TR\|</\?TH' | 
+	gsed 's/^[\ \t]*//g' |
+	tr -d '\n\r' |
+	gsed 's/<\/TR[^>]*>/\n/Ig' |
+	gsed 's/<\/\?\(TABLE\|TR\)[^>]*>//Ig' |
+	gsed 's/^<T[DH][^>]*>\|<\/\?T[DH][^>]*>$//Ig' |
+	gsed 's/<\/T[DH][^>]*><T[DH][^>]*>/;/Ig' |
+	tail -n +2 |
+	gsed 's/^;//g'
 
 unset _PHONEID
 unset _DEVICEID
