504f46a1741010ad136e0896842d4625bcc5c2ba
Michael Mäder <mike@moik.org>
2016-06-21 07:56:32 +0200

    script writes data to sqlite DB

--- get-Temperatures.sh
+++ get-Temperatures.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-_FROM=$(date -v-1H "+%d.%m.%Y+%H:00:00") 
+_FROM=$(date  -v-2d -v-1H "+%d.%m.%Y+%H:00:00")
 _TO=$(date -v-1H "+%d.%m.%Y+%H:59:59")
 _FROME=$(date -j -f "%d.%m.%Y+%H:%M:%S" $_FROM "+%s")
 _TOE=$(date -j -f "%d.%m.%Y+%H:%M:%S" $_TO "+%s")
+_OUTFILE=./out.csv
 
 echo $_FROM
 echo $_TO
-echo $_FROME
-echo $_TOE
+#echo $_FROME
+#echo $_TOE
 
 source ./credentials.sh
+_DEVICEID=$_DEVICEID1
 
-curl -i -s -k  -X 'POST' \
+curl -s -i -k  -X 'POST' \
 	-b "PhoneID=$PHONEID" \
  	--data-binary $"deviceid=$_DEVICEID&vendorid=C7BB7FBE-BCEB-48D0-9A49-764A24A6E545&appbundle=de.synertronixx.remotemonitor&fromepoch=$_FROME&toepoch=$_TOE&from=$_FROM&to=$_TO&pagesize=250"\
  	'http://measurements.mobile-alerts.eu/Home/MeasurementDetails' | 
-	grep -i -e '</\?TABLE\|</\?TD\|</\?TR\|</\?TH' | 
+	grep -i -e '</\?TABLE\|</\?TD\|</\?TR\|</\?TH' |
 	gsed 's/^[\ \t]*//g' |
 	tr -d '\n\r' |
 	gsed 's/<\/TR[^>]*>/\n/Ig' |
@@ -24,8 +26,11 @@ curl -i -s -k  -X 'POST' \
 	gsed 's/^<T[DH][^>]*>\|<\/\?T[DH][^>]*>$//Ig' |
 	gsed 's/<\/T[DH][^>]*><T[DH][^>]*>/;/Ig' |
 	tail -n +2 |
-	gsed 's/^;//g'
+	gsed 's/ mm//g' |
+	gsed 's/\([0-9][0-9]\).\([0-9][0-9]\).\([0-9][0-9][0-9][0-9]\)/\3-\2-\1/' | # put the date in the right format
+	gsed 's/^;//g' > $_OUTFILE
 
 unset _PHONEID
 unset _DEVICEID
 
+echo -e ".separator ';' \n.import $_OUTFILE rain_sensor" | sqlite3 /root/bin/mobilealerts/mobilealerts.db
